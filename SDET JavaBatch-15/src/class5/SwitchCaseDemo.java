99bfea949f8a650b2f1cdd1433722578cfe0d874
C9198-Mete <mttrkgl1907@gmail.com>
2023-01-07 15:01:52 -0500

    SwitchCase  added

--- SDET JavaBatch-15/src/class5/SwitchCaseDemo.java
+++ SDET JavaBatch-15/src/class5/SwitchCaseDemo.java
@@ -3,7 +3,7 @@ package class5;
 public class SwitchCaseDemo {
     public static void main(String[] args) {
 
-        int day = 1;
+        int day = 7;
         switch (day) {
             case 1:
                 System.out.println("Monday");
