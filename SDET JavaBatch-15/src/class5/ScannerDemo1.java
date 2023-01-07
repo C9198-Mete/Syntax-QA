41b8d617d4c50bdddf160bd9675a25088c487481
C9198-Mete <mttrkgl1907@gmail.com>
2023-01-07 10:41:05 -0500

    scanner added

--- SDET JavaBatch-15/src/class5/ScannerDemo1.java
+++ SDET JavaBatch-15/src/class5/ScannerDemo1.java
@@ -1,7 +1,12 @@
 package class5;
 
+import java.util.Scanner;
+
 public class ScannerDemo1 {
     public static void main (String[] args){
+
+        Scanner scan=new Scanner(System.in);
+
 
     }
 }
