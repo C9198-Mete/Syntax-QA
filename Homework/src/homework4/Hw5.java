796bd65aba010aac71913ec10df56fe64fb46a29
C9198-Mete <mttrkgl1907@gmail.com>
2023-01-08 11:13:00 -0500

    homework5 changed

--- Homework/src/homework4/Hw5.java
+++ Homework/src/homework4/Hw5.java
@@ -4,17 +4,18 @@ import java.util.Scanner;
 
 public class Hw5 {
     public static void main(String[] args){
-
-        double num1=40;
-        double num2=50;
-        double num3=30;
+        Scanner input=new Scanner(System.in);
+        System.out.println("Please enter three number");
+        double num1=input.nextDouble();
+        double num2=input.nextDouble();
+        double num3=input.nextDouble();
 
         if (num1>num2 && num1>num3){
-            System.out.println("The largest number is "+ num1);
+            System.out.println("The largest number is number1 "+ num1);
         }else if (num2>num1 && num2>num3){
-            System.out.println("The largest number is "+ num2);
+            System.out.println("The largest number is number2 "+ num2);
         }else {
-            System.out.println("The largest number is "+ num3);
+            System.out.println("The largest number is number3 "+ num3);
         }
     }
 }
