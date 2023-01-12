75f6ee7a79d9e1b0d258590d76d905cb53fc6180
MuhammedAkcin <mttrkgl1907@gmail.com>
2023-01-11 19:26:14 -0500

    WhileLoopRecap added

---
+++ SDET JavaBatch-15/src/class7/WhileRecap.java
@@ -0,0 +1,11 @@
+package class7;
+
+public class WhileRecap {
+    public static void main(String[] args) {
+        int num=10;
+        while (num<20){
+            System.out.println(num+ " is still less than 20");
+            num++;
+        }
+    }
+}
