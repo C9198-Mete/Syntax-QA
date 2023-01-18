cbc9a2e5d66707bc9f14f0ede7724fe3c7a0e875
MuhammedAkcin <mttrkgl1907@gmail.com>
2023-01-17 19:31:34 -0500

    homework7 changed

--- Homework/src/homework7/Task6.java
+++ Homework/src/homework7/Task6.java
@@ -3,10 +3,10 @@ package homework7;
 public class Task6 {
     public static void main(String[] args) {
 
-        String[][] countries={{"USA","England","Turkey","Spain","France"},{"Washington","London","Ankara","Madrid","Paris"}};
+        String[][] countries={{"Finland","Norway","Denmark","Crotia","Austria"},{"Helsinki","Oslo","Copenhagen","Zagreb","Vienna"}};
 
             for (int j =0; j <5 ; j++) {
-                System.out.println(countries[1][j]+" is ca[ital of "+ countries[0][j]);
+                System.out.println(countries[1][j]+" is capital of "+ countries[0][j]);
             }
         }
     }
---
+++ SDET JavaBatch-15/src/class10/ArrayDemo1.java
@@ -0,0 +1,7 @@
+package class10;
+
+public class ArrayDemo1 {
+    public static void main(String[] args) {
+
+    }
+}
