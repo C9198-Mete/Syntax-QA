3012f142323bb3f942c13a8c9dd8ce7f564a3923
MuhammedAkcin <mttrkgl1907@gmail.com>
2023-01-17 19:41:46 -0500

    class10 added

--- SDET JavaBatch-15/src/class10/ArrayDemo1.java
+++ SDET JavaBatch-15/src/class10/ArrayDemo1.java
@@ -3,5 +3,20 @@ package class10;
 public class ArrayDemo1 {
     public static void main(String[] args) {
 
+        int[] nums={1,2,3,4,5};
+        System.out.println(nums[4]);
+
+
+        for (int i = 0; i < nums.length; i++) {
+            System.out.println(nums[i]);
+        }
+
+        System.out.println("****************");
+
+        for (int i: nums) {
+            System.out.println(i);
+        }
+
+
     }
 }
