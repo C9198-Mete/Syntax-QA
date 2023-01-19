cc016b1c34711acbd9279f84faafeb8ab43359c7
MuhammedAkcin <mttrkgl1907@gmail.com>
2023-01-18 19:23:24 -0500

    class11 added

--- SDET JavaBatch-15/src/class10/D2Arrays2.java
+++ SDET JavaBatch-15/src/class10/D2Arrays2.java
@@ -10,9 +10,10 @@ public class D2Arrays2 {
 
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
-                System.out.println(matrix[i][j]);
+                System.out.print(matrix[i][j]+" ");
 
             }
+            System.out.println();
 
         }
 
---
+++ SDET JavaBatch-15/src/class11/D2Array.java
@@ -0,0 +1,10 @@
+package class11;
+
+public class D2Array {
+    public static void main(String[] args) {
+        int [][] matrix={{10,20,30},{40,50,60,25,55},{70,80,90,100}};
+
+        System.out.println(matrix[1][3]);// output is 25
+
+    }
+}
